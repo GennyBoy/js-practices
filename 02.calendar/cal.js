e2a52b55fe018877303681357fd4f05f00b55847
GennyBoy <tassie2856@gmail.com>
2022-11-17 08:39:46 +0900

    曜日を意識した表示にした

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -7,13 +7,23 @@ dayjs.locale(ja);
 let today = dayjs();
 let year = today.format("YYYY");
 let month = today.format("MMM");
-let last_day = today.endOf("month").format("DD");
+let last_day_of_month = today.endOf("month").format("DD");
+let day_of_week_for_first_day_of_month = dayjs().startOf("month").day();
 
 // 月と年を表示する
-// 1月 2022
 console.log(`    ${month} ${year}`);
+console.log("日 月 火 水 木 金 土");
 
-// 1日から月末までの日を出力する
-for (let i = 1; i <= last_day; i++) {
+// 月初の曜日まで空白を入れる
+for (let i = 0; i <= day_of_week_for_first_day_of_month; i++) {
+  process.stdout.write("  ");
+}
+
+let wday = day_of_week_for_first_day_of_month;
+for (let i = 1; i <= last_day_of_month; i++) {
   process.stdout.write(`${i} `);
+  if (wday == 6) {
+    process.stdout.write("\n");
+  }
+  wday == 6 ? (wday = 0) : wday++;
 }
