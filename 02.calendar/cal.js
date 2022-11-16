9aaa76a2e6b085422920f0ec3e3712c844e35353
GennyBoy <tassie2856@gmail.com>
2022-11-17 06:42:12 +0900

    1日から月末までの日を出力できるようにした

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -7,8 +7,13 @@ dayjs.locale(ja);
 let today = dayjs();
 let year = today.format("YYYY");
 let month = today.format("MMM");
-// let day = today.format("DD");
+let last_day = today.endOf("month").format("DD");
 
 // 月と年を表示する
 // 1月 2022
 console.log(`    ${month} ${year}`);
+
+// 1日から月末までの日を出力する
+for (let i = 1; i <= last_day; i++) {
+  process.stdout.write(`${i} `);
+}
