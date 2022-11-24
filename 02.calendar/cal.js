af275817cd2053f796f9a6ee267fa7f15dad65a5
GennyBoy <tassie2856@gmail.com>
2022-11-25 07:26:26 +0900

    使えるところはdayjsのメソッドをできるだけ使うようにした

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -3,11 +3,12 @@
 const dayjs = require("dayjs");
 const ja = require("dayjs/locale/ja");
 const argv = require("minimist")(process.argv.slice(2));
+const utc = require("dayjs/plugin/utc");
+dayjs.extend(utc);
 
-const year = argv.y || dayjs().format("YYYY");
-const month = argv.m || dayjs().format("MM");
-const date = dayjs(`${year}-${month}`, "YYYY-MM");
-const last_day_of_month = date.daysInMonth();
+const year = argv.y || dayjs.utc().format("YYYY");
+const month = argv.m || dayjs.utc().format("MM");
+const date = dayjs.utc(`${year}-${month}`, "YYYY-MM");
 const day_of_week_for_first_day_of_month = date.startOf("month").day();
 
 // 月と年を表示する
@@ -19,15 +20,15 @@ for (let i = 0; i < day_of_week_for_first_day_of_month; i++) {
   process.stdout.write("   ");
 }
 
-let wday = day_of_week_for_first_day_of_month;
-for (let i = 1; i <= last_day_of_month; i++) {
+let date_index = date.startOf("month");
+while (date_index.isBefore(date.endOf("month"))) {
   // 1桁の日の時は空白で埋める
-  process.stdout.write(`${i.toString().padStart(2, " ")} `);
+  process.stdout.write(`${date_index.date().toString().padStart(2, " ")} `);
   // 土曜日になったら改行を入れる
-  if (wday == 6) {
+  if (date_index.day() == 6) {
     process.stdout.write("\n");
   }
-  wday == 6 ? (wday = 0) : wday++;
+  date_index = date_index.add(1, "day");
 }
 
 // これをしないと出力結果の末尾に `%` が表示されてしまう
