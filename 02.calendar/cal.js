e3d8cba3cdd5cb183aa45e800dd66a3e870a9f8a
GennyBoy <tassie2856@gmail.com>
2022-11-25 05:25:42 +0900

    const使えるところはconstで定義する

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,14 +1,14 @@
 #! /usr/bin/env node
 
-let dayjs = require("dayjs");
-let ja = require("dayjs/locale/ja");
-let argv = require("minimist")(process.argv.slice(2));
+const dayjs = require("dayjs");
+const ja = require("dayjs/locale/ja");
+const argv = require("minimist")(process.argv.slice(2));
 
-let year = argv.y || dayjs().format("YYYY");
-let month = argv.m || dayjs().format("MM");
-let date = dayjs(`${year}-${month}`, "YYYY-MM");
-let last_day_of_month = date.daysInMonth();
-let day_of_week_for_first_day_of_month = date.startOf("month").day();
+const year = argv.y || dayjs().format("YYYY");
+const month = argv.m || dayjs().format("MM");
+const date = dayjs(`${year}-${month}`, "YYYY-MM");
+const last_day_of_month = date.daysInMonth();
+const day_of_week_for_first_day_of_month = date.startOf("month").day();
 
 // 月と年を表示する
 console.log(`     ${date.locale(ja).format("MMM")} ${date.format("YYYY")}`);
