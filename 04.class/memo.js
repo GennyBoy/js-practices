6610b480f09a735f9209d43e7c20922bc2bc65dd
GennyBoy <tassie2856@gmail.com>
2022-11-26 19:46:52 +0900

    保存しているメモを読み込めるようにした

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -2,15 +2,23 @@
 const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 
-const input = fs.readFileSync("/dev/stdinddd", "utf8");
+// const input = fs.readFileSync("/dev/stdin", "utf8");
 
 class Memo {
-  constructor(content) {
-    this.content = content;
+  constructor(body, id = null) {
+    this.id = id;
+    this.body = body;
+  }
+
+  read(id) {
+    const file = JSON.parse(fs.readFileSync(`database/${id}.json`));
+    this.body = file.body;
+    console.log(this.body);
   }
 
   save() {
-    const file_name = `${uuidv4()}.text`;
+    this.id = uuidv4();
+    const file_name = `${this.id}.json`;
     fs.writeFile(`database/${file_name}`, JSON.stringify(this), (err) => {
       if (err) throw err;
       console.log("正常に書き込みが完了しました");
@@ -18,5 +26,7 @@ class Memo {
   }
 }
 
-const memo = new Memo(input);
-memo.save();
+// const memo = new Memo(input);
+// memo.save();
+const memo = new Memo();
+memo.read("b8f23274-e34d-45b0-8270-c6aed6d65497");
