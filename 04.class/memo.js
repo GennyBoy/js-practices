27aa025325937a00d4ede0a639a1537c99072e81
GennyBoy <tassie2856@gmail.com>
2022-11-26 18:13:56 +0900

    メモを追加できるようにした

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,4 +1,22 @@
 #! /usr/bin/env node
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
 
-const input = require("fs").readFileSync("/dev/stdin", "utf8");
-console.log(input);
+const input = fs.readFileSync("/dev/stdinddd", "utf8");
+
+class Memo {
+  constructor(content) {
+    this.content = content;
+  }
+
+  save() {
+    const file_name = `${uuidv4()}.text`;
+    fs.writeFile(`database/${file_name}`, JSON.stringify(this), (err) => {
+      if (err) throw err;
+      console.log("正常に書き込みが完了しました");
+    });
+  }
+}
+
+const memo = new Memo(input);
+memo.save();
