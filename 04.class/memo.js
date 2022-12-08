46c03496ccf1129b4233bc7f5e9a574af4f80d8e
GennyBoy <tassie2856@gmail.com>
2022-12-09 05:30:13 +0900

    一旦クラスを使わずに実装することにした
    いきなりクラス化を考えると、全く実装が進まなくなったので一度クラスではない形で機能実装することにした

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,32 +1,50 @@
 #! /usr/bin/env node
 const { v4: uuidv4 } = require("uuid");
-const fs = require("fs");
+const fs = require("node:fs/promises");
+const argv = require("minimist")(process.argv.slice(2));
 
-// const input = fs.readFileSync("/dev/stdin", "utf8");
-
-class Memo {
-  constructor(body, id = null) {
-    this.id = id;
-    this.body = body;
-  }
-
-  read(id) {
-    const file = JSON.parse(fs.readFileSync(`database/${id}.json`));
-    this.body = file.body;
-    console.log(this.body);
-  }
-
-  save() {
-    this.id = uuidv4();
-    const file_name = `${this.id}.json`;
-    fs.writeFile(`database/${file_name}`, JSON.stringify(this), (err) => {
-      if (err) throw err;
-      console.log("正常に書き込みが完了しました");
-    });
+async function listFirstLines() {
+  try {
+    const files = await fs.readdir("database/");
+    for (const file of files) {
+      const memo_json = await fs.readFile(`database/${file}`, {
+        encoding: "utf-8",
+      });
+      const memo_obj = JSON.parse(memo_json);
+      console.log(memo_obj.first_line);
+    }
+  } catch (err) {
+    console.error(err);
   }
 }
 
-// const memo = new Memo(input);
-// memo.save();
-const memo = new Memo();
-memo.read("b8f23274-e34d-45b0-8270-c6aed6d65497");
+async function readInput() {
+  return fs.readFile("/dev/stdin", "utf8");
+}
+
+async function createMemo(body) {
+  const id = uuidv4();
+  const json_body = {
+    id: id,
+    first_line: body.split(/\n/)[0],
+    body: body,
+  };
+
+  const file_name = `${id}.json`;
+  fs.writeFile(`database/${file_name}`, JSON.stringify(json_body), (err) => {
+    if (err) throw err;
+    console.log("正常に書き込みが完了しました");
+  });
+}
+
+if (argv.l) {
+  listFirstLines();
+} else {
+  readInput()
+    .then((value) => {
+      createMemo(value);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
