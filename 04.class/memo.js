98a994a9b55e05556332d8977ac65221cc16e822
GennyBoy <tassie2856@gmail.com>
2022-12-09 05:38:16 +0900

    -d オプションを実装

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -2,6 +2,7 @@
 const { v4: uuidv4 } = require("uuid");
 const fs = require("node:fs/promises");
 const argv = require("minimist")(process.argv.slice(2));
+const { Select } = require("enquirer");
 
 async function listFirstLines() {
   try {
@@ -37,8 +38,48 @@ async function createMemo(body) {
   });
 }
 
+async function fetchAllMemoObj() {
+  let memo_objects = [];
+
+  const files = await fs.readdir("database/");
+
+  for (const file of files) {
+    const memo_json = await fs.readFile(`database/${file}`, {
+      encoding: "utf-8",
+    });
+    memo_objects.push(JSON.parse(memo_json));
+  }
+  return memo_objects;
+}
+
+async function buildChoicesForPrompt() {
+  let choices = [];
+  const memo_objects = await fetchAllMemoObj();
+  memo_objects.forEach((memo_obj) => {
+    const choice = {
+      name: memo_obj.id,
+      message: memo_obj.first_line,
+      value: memo_obj.first_line,
+    };
+    choices.push(choice);
+  });
+  return choices;
+}
+
+const deletePrompt = new Select({
+  name: "note",
+  message: "Choose a note you want to delete:",
+  choices: buildChoicesForPrompt(),
+});
+
 if (argv.l) {
   listFirstLines();
+} else if (argv.d) {
+  deletePrompt.run().then((answer) => {
+    fs.rm(`database/${answer}.json`).then(() => {
+      console.log("deleted the file");
+    });
+  });
 } else {
   readInput()
     .then((value) => {
